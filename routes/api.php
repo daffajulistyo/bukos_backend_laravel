b7883bb962b459be05d1c0a9b3057d17dae5e9b5
ryansyah17 <ryansyahputra553@gmail.com>
2021-11-18 16:02:45 +0700

    Login by katak

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', [UserController::class, 'register']);
-  
+Route::post('login', [UserController::class, 'login']);
 
 
 
