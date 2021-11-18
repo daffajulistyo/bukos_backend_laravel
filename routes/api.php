55c85ec217fd908db655ff5dfb158ffad5778325
ryansyah17 <ryansyahputra553@gmail.com>
2021-11-18 16:45:58 +0700

    fetch, update, logout done

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
+});
 
 
 // //API route for register new user
